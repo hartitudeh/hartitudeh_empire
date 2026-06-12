0ec055d80b138f18eba757ab06592815c1674508
pelumi adeyemi <pelumiadeyemi111@gmail.com>
2026-06-12 15:57:25 +0100

    Landing page error fixed

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -49,7 +49,7 @@ export default function Navbar() {
             <img 
               src={logoFull} 
               alt="Hartitudeh Empire Logo" 
-              className="h-14 md:h-16 w-auto object-contain" 
+              className="h-10 md:h-12 w-auto object-contain" 
             />
           </Link>
 
